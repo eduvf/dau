77762e2b9b4a9d7368379da92f14bdaba1752790
EduVF <eduardvf@outlook.com>
2023-07-21 21:37:38 +0200

    minimal

--- dau.js
+++ dau.js
@@ -1,127 +1,86 @@
 // @ts-check
 
-const test0 = `
-; 100 doors
+const test = `
+; factorial using range
+(: (f n) (* (..= 1 n)))
 
-"hi""ho""tes\\"t"
-10 10.10 10.e 0x123ff 0b01102
-😂😃(🧘🏻‍♂️ 🌍 🌦️) 🥖.🚗
-fàstic
-
-(open = ('))
-(@ 'i 1 100
-    (@ 'j i 100 i
-        (open.j = !open.j)))
-
-(@ 'i 'v open
-    (>> "door " & i & " is " &
-        (? v "open" "close")))
-`;
-
-const test1 = `
-(: (f n) (* 1..=n)
-
+; using a simple loop
 (: (f n)
     (= r "")
-    (@ (' i 2 n)
+    (@ i 2 n
         (*= r i)))
 
+; using recursion
 (: (f n)
-    (? n <= 1
+    (? (<= n 1)
         1
-        n * (f n-1)))
-
-(pr (f 5))
-`;
-
-const text2 = `
-grad = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '$', '@']
-@ y, -1, 1, 0.05
-	line = ''
-	@ x, -1.5, 1.5, 0.025
-		a = x
-		b = y
-		i = 0
-		@ i < 100
-			t = a
-			a = a * a - b * b - 0.79
-			b = t * b * 2 + 0.15
-			? (a * a + b * b) > 4
-				/>
-			,	i += 1
-		line &= grad[flr i/10]
-	pr line
-`;
-
-/*
-let map = [" ", ".", ":", "-", "=", "+", "*", "#", "%", "$", "@"];
-for (let y = -1; y < 1; y += 0.05) {
-	let line = '';
-	for (let x = -1.5; x < 1.5; x += 0.025) {
-		let [zr, zi, i] = [x, y, 0];
-		while (i < 100) {
-			let old_zr = zr;
-			zr = zr * zr - zi * zi - 0.79;
-			zi = old_zr * zi * 2 + 0.15;
-			if (zr * zr + zi * zi > 4) break;
-			else i++;
-		}
-		line += map[Math.floor(i / 10)];
-	}
-	console.log(line);
-}
-//*/
-
-const test3 = `
-(: (f n)
-    (? n <= 1
-        1
-        n * (f n - 1x)))
+        (* n (f (- n 1)))))
 
 (pr "5! = " (f 5))
 `;
 
-dau(test3);
+const test2 = `
+(+ 1 2 3)
+`;
 
-//--------------------------------------------------------------
-
-var input_string;
-
-function dau(input, env) {
-	input_string = input;
-	console.log(Array.from(lex(input)));
+function dau(input) {
+	let r = parse(lex(input));
+	console.dir(r, { depth: null });
+	console.log(evaluate(r));
 }
 
-function error(type, i, t) {
-	const line = input_string.slice(0, i).split('\n').length;
-	const msg = {
-		str: 'Unterminated string literal',
-		num: 'Invalid number "' + t + '"',
-	}[type];
-
-	console.error('⚠️  Error at line ' + line + ': ' + msg);
-	return null;
+function lex(str) {
+	const re = /;.*|".*?"|[()']|[^()'"\s]+/g;
+	return str.match(re).filter((t) => !t.startsWith(';'));
 }
 
-function* lex(s) {
-	const re = /;.*|"(?:\\.|[^"])*"?|[^()'\s]+|\S/g;
+function identify(t) {
+	if (t.startsWith('"')) return { lit: t.slice(1, -1) };
+	if (!isNaN(t)) return { lit: Number(t) };
+	return { ref: t };
+}
 
-	const str = (t) => (t.endsWith('"') ? t : error('str'));
-	const num = (t, i) => (!isNaN(t) ? Number(t) : error('num', i, t));
-
-	for (let token of s.matchAll(re)) {
-		const t = token[0];
-		const i = token.index;
-
-		// comment
-		if (t[0] === ';') continue;
-		// parenthesis
-		if (t === '(' || t === ')') yield [t];
-		// string
-		else if (t[0] === '"') yield ['s', i, str(t)];
-		// number
-		else if (/^-?\d/.test(t)) yield ['n', i, num(t, i)];
-		// identifier
-		else yield ['i', i, t];
+function parse(tok, tree = []) {
+	while (tok.length > 0) {
+		const t = tok.shift();
+		if (t === ')') break;
+		if (t === '(') {
+			const branch = [parse(tok)];
+			return parse(tok, tree.concat(branch));
+		}
+		tree.push(identify(t));
 	}
+	return tree;
 }
+
+const lib = {
+	'+': (...xs) => xs.reduce((a, b) => a + b, 0),
+	'*': (...xs) => xs.reduce((a, b) => a * b, 1),
+	'%': (...xs) => xs.reduce((a, b) => a % b, Infinity),
+	'-': (...xs) => (xs.length !== 1 ? xs.reduce((a, b) => a - b) : -xs[0]),
+	'/': (...xs) => (xs.length !== 1 ? xs.reduce((a, b) => a / b) : 1 / xs[0]),
+	'::': (...xs) => [...xs],
+	'->': (x) => x,
+	pr: (x) => (console.log(x), x),
+};
+
+function get(sym, env, i = env.length - 1) {
+	if (sym in env[i]) return env[i][sym];
+	return i > 0 ? get(sym, env, i - 1) : null;
+}
+
+function evaluate(node, env = [lib]) {
+	if (Array.isArray(node)) {
+		if (!node.length) return null;
+
+		const list = node.map((x) => evaluate(x, env));
+		if (list[0] instanceof Function) {
+			const [fn, ...args] = list;
+			return fn(...args);
+		}
+		return list[list.length - 1];
+	}
+	return node.ref ? get(node.ref, env) : node.lit;
+}
+
+dau(test2);
