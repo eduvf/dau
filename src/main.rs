492527f837cfa39a46d31fe547af155b387cbe30
EduVF <eduardvf@outlook.com>
2023-08-07 18:46:28 +0200

    read argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        1 => todo!(),
+        2 => {
+            match fs::read_to_string(&args[1]) {
+                Ok(s) => println!("{}", s),
+                Err(e) => println!("{}", e),
+            };
+        }
+        _ => todo!(),
+    }
 }
